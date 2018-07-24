ef7e9459c6b0bc91897d6072bfb2ae7833feb6c8
SnowSpoki <41550604+SnowSpoki@users.noreply.github.com>
2018-07-24 13:34:07 +0300

    Add files via upload

---
+++ LoadSceneOnClick.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour {
+	public void LoadbyIndex (int sceneIndex)
+	{
+		SceneManager.LoadScene (sceneIndex);
+	}
+}
