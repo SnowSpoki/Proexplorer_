60689f8df5f417189be4ba43db6eafc97fba4ef1
SnowSpoki <41550604+SnowSpoki@users.noreply.github.com>
2019-12-04 12:53:50 +0200

    Create Lighting.cs

---
+++ Lighting.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.EventSystems;	
+using UnityEngine.UI;	public class LightingSwitcher : MonoBehaviour
+	{
+ {
+    static double LightingSwitcher
+    
+    static void Main()
+    {
+
+        try
+        {
+            RenderSettings.skybox = skyBoxMaterial;
+            lightingGameObject.SetActive(true);        }
+        catch (lightingGameObject)
+        {
+            camera.SetActive(false);
+        }
+    }
+}
