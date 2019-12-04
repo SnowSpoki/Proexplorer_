9fa095454bdc92f2150746bda526cb21edf64db2
SnowSpoki <41550604+SnowSpoki@users.noreply.github.com>
2019-12-04 12:52:22 +0200

    Create Knight.cs

---
+++ Knight.cs
@@ -0,0 +1,19 @@
+using Unity;
+using System.Collection;
+
+public class ActiveStateToggler : MonoBehaviour {
+   public string Woman 
+   { get; set; }
+
+   public decimal Weapon
+   { get; set; }
+}
+
+class Program
+{
+   static void Main(string[] args)
+   {
+      var item = new Object{ Name = "Sword", Weapon = 2.2m };
+      gameObject.SetActive (!gameObject.activeSelf);
+   }
+}
